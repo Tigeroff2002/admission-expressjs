a0a6455c436b07b5fb4e2e03894143060057a58c
Kirill <parahinkv@gmail.com>
2024-10-06 21:57:52 +0300

    fix db failures

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const { Pool } = require('pg');
+
+require('dotenv').config();
+
+const pool = new Pool({
+    user: 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
 const app = express();
 const port = 3030;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/abiturients', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM abiturients');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
 });
 
 app.listen(port, () => {
